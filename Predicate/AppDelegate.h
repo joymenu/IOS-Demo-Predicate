edc87b219166e0fdb3d330b66b9eebc32d9430f0
卜错 <baiming.org@gmail.com>
2015-02-27 10:41:56 +0800

    Initial Commit

---
+++ Predicate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Predicate
+//
+//  Created by bbu on 2/27/15.
+//  Copyright (c) 2015 bbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
